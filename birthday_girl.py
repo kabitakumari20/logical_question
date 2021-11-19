cea7f95d0f6c122a98f9c460d54e6501d1b7e569
kabita kumari <74282784+kabitakumari20@users.noreply.github.com>
2021-11-19 22:20:10 +0530

    Add files via upload

---
+++ birthday_girl.py
@@ -0,0 +1,49 @@
+birthday=input("Enter the day:= ")
+name=input("Enter the Name:=")
+if birthday=="monday":
+    if name=="manvi":
+        print("HAPPY BIRTHDAY DEAR MANVI 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+    else:
+        print("no birthda")
+elif birthday=="tuesday":
+    if name=="ujala":
+        print("HAPPY BIRTHDAY DEAR UJALA 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+    else:
+        print("no birthday")
+elif birthday=="wednesday":
+    if name=="suman":
+        print("HAPPY BIRTHDAY DEAR SUMAN 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+    else:
+        print("no birthday")
+elif birthday == "thrusday":
+    if name=="savita":
+        print("HAPPY BIRTHDAY DEAR SAVITA 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+    else:
+        print("sdfghjkl")
+elif birthday == "friday":
+    if name=="anjum":
+        print("HAPPY BIRTHDAY DEAR ANJUM 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+    else:
+        print("zxcvbnm,")
+elif birthday=="saturday":
+    if name=="amla":
+        print("HAPPY BIRTHDAY DEAR AMLA 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+    else:
+        print("vvvvvvvv")
+elif birthday=="sunday":
+    if name=="mom":
+        print("HAPPY BIRTHDAY DEAR MOM 🎈🎈🎉🎉🎂")
+        print("AGIN I AM WISHING YOU HAPPY BIRTHDAY 🎈🎈🎉🎉🎂")
+        print("🌹🌹🌹🌹🌹🌹🌹🌹")
+        print("❤️❤️❤️❤️❤️❤️")
+    else:
+        print("nnnnnnnnnnnnnnnnnn")
+
+else:
+    print("day is wrong")
